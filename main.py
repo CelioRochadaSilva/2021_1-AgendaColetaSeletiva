43f14b242873412b2adcda40964f109eb8c0ddd7
Celio Rocha <40038619+CelioRochadaSilva@users.noreply.github.com>
2022-05-15 16:27:59 -0300

    Alterações no main.
    correções no arquivos main

--- main.py
+++ main.py
@@ -1,207 +1,217 @@
-#inicio
+import os
 
-cliente=0 
-coleta=0
-tcoleta=0
-pcoleta=0
-#lcoleta=0
-#salvar_diario=0 
-
-ldiario=[cliente,tcoleta,pcoleta]
 print('=+' * 20)
 print('Sejam Bem vindo(a)!')
 print('Coleta seletiva Pessoa Fisica!')
 
 def menu (): 
-    print ('Escolha uma opção de 1 a 7 \n') 
-    print ('   1 - Cadastrar cliente') # opção de cadastro para cliente com a função adicionar_cadastro
-    print ('   2 - Remover cliente') # remove cliente cadastrado com função remover_cadastro
-    print ('   3 - Listar cliente') # lista clientes cadastrado na lista
-    print ('   4 - Cadastrar coleta')
-    print ('   5 - Listar coleta')
-    print ('   6 - Excluir coleta')
-    print ('   7 - Sair\n') # fecha o aplicativo
-    opt = input('Digite a opçao desejada: utf8') # entrada de dados
+    print ('\nEscolha uma opção de 1 a 7 \n') 
+    print ('   |1| - Cadastrar cliente') # opção de cadastro para cliente com a função adicionar_cadastro
+    print ('   |2| - Remover cliente') # remove cliente cadastrado com função remover_cadastro
+    print ('   |3| - Listar cliente') # lista clientes cadastrado na lista
+    print ('   |4| - Cadastrar coleta')#cadastra coleta
+    print ('   |5| - Listar coleta') #lista coletas cadastrada
+    print ('   |6| - Excluir coleta')# ecluir coleta cadastrada
+    print ('   |7| - Sair\n') # fecha o aplicativo
+    opt = input('Digite a opçao desejada: ').strip() # entrada de dados
     print('=+' * 20)
-    return opt 
-def adicionar_cliente():
+    return opt # retorna menu opçes
+    
+def adicionar_cliente():#adicionar cadastro cliente
     lcliente=[]
-    cliente = str(input("Digite o nome do cliente:"))
+    cliente = str(input('Digite o nome do cliente: ')).strip()
     cliente=cliente.upper()
     lcliente.append(cliente) # adiciona cliente na lista
-    f = open("cliente.txt", "a") #w apaga a pode escrever
-    # print(f.read())
-    # print(f.readline())
-    # print(f.readline())
+    f = open("cliente.txt", "a", encoding='utf-8') #w apaga a pode escrever
+    
     f.write((cliente))
     f.close()
 
     if cliente in lcliente:
-        print('Cadastro realizado', cliente)
+        print('Cadastro realizado com sucesso !', cliente, ' ',) #, end='|'
+        # lcliente = cliente  implmentação quantidade clientes 
+        # cliente=cliente + 1
     else:
                       
         print('Cliente com cadastrado') 
-    pass 
+    #pass 
 
 def remover_cliente():
     lcliente=[]
-    cliente=str(input("Digite o nome do cliente:"))
-    cliente=cliente.upper()
-    f = open("cliente.txt", "w") #w apaga a pode escrever
-    # print(f.read())
-    #print(cliente.readline())
-    # print(f.readline())
-    f.write((cliente))
-    f.close()
-    
-    if cliente in lcliente:
-        print('Cliente Removido',cliente)
-    else:
-        print('Esse cliente não está cadastrado') 
-    pass 
+    # contatoEncontrado = False
+    if(os.path.isfile('cliente.txt') == True):
+        cliente = str(input('Digite o nome do cliente: ')).strip()
+        cliente=cliente.upper() # retorna string para maiuscula
+        
+    with  open("cliente.txt", "r")as arquivo: #w apaga a pode escrever
+        linhas = arquivo.readlines()
 
+    with  open("cliente.txt", "w")as arquivo: # utlizado para escrever no arquivo
+        for contato in linhas:
+            lcliente.append(contato) # add contato
+            lcliente = lcliente[0].split()
+            if lcliente != cliente:
+                print('Cliente Excluido')
+                #arquivo.write(cliente)
+            else:
+                print('Cliente OK')
+     
+      
 def listar_clientes():
     #lcliente=[] 
-    f = open("cliente.txt", "r") # não exibe lista de clientes
-   
-    #print(f.readlines())
+    f = open("cliente.txt", "r", encoding='utf-8') # não exibe lista de clientes
     clientes = f.readlines()
     for cliente in clientes:
-        print(cliente)
+        print(cliente , end='| \n')
     f.close()
-    # for contato in clientes:
-    #     clientes.append(contato)
-    #     lista = lista[0].split(';')
-    # if (lista[0] == listar_clientes):
-   
     
 def adicionar_coleta():
-    tcoleta=[]
-    f = open("coleta.txt", "a") # não exibe lista de clientes
+    lcliente=[]
+    cliente = str(input("Digite o nome do cliente: "))
+    cliente=cliente.upper()
+    lcliente.append(cliente) # adiciona cliente na lista
+    f = open("cliente.txt", "a", encoding='utf-8') #w apaga a pode escrever
+    f.write((cliente))
+    f.close()
+           
+    f = open("coleta.txt", "a", encoding='utf-8') #  retirei a e col w não exibe lista de clientes
     
     print ('Escolha a opção de tipo coleta:') 
-    print ('   1 - Papel') 
-    print ('   2 - Vidro') 
-    print ('   3 - Metal') 
-    print ('   4 - Orgânico')
-
-    coleta = str(input("Digite o tipo de coleta:"))
+    print ('   |1ª| - Papel') 
+    print ('   |2ª| - Vidro') 
+    print ('   |3ª| - Metal') 
+    print ('   |4ª| - Orgânico')
+    
+    tcoleta=[]
+    print ('Digite sua opção: 1 a 4')
+    print()
+    coleta = str(input('Digite o tipo de coleta: '))
     coleta=coleta.upper()
     tcoleta.append(coleta)
-    #print(f.readline())
-    #clientes = f.readlines()
-    f.write((coleta))
     f.close()   
     
     if coleta == '1':
-        coleta='Papel' # intenção adicionar texto
+        coleta = 'Papel' # intenção adicionar texto
         tcoleta.append(coleta) # falta inclementar 
-        #print('Opção de solicitação registrada', tcoleta)
+        print('Opção de coleta registrada: ', coleta)
     
     elif coleta == '2':
-        coleta='Vidro' # intenção adicionar texto
+        coleta ='Vidro' # intenção adicionar texto
         tcoleta.append(coleta)
-        print('Opção de solicitação de coleta registrada')
+        print('Opção de solicitação: registrada! ', coleta)
     
     elif coleta == '3':
         coleta='Metal' # intenção adicionar texto
         tcoleta.append(coleta)
-        print('Opção de solicitação de coleta registrada')
+        print('Opção de solicitação: registrada! ', coleta)
     
     elif coleta == '4':
         coleta='Metal' # intenção adicionar texto
         tcoleta.append(coleta)
-        print('Atualização registrada')
+        print('Opção de solicitação: registrada! ', coleta)
             
     else:
              
-        print('atualização de coleta não registrada') 
-    pass 
+        print('Opção de solicitação: não registrada! ') 
+    #pass 
     
+    f = open("horacoleta.txt", "a", encoding='utf-8') # não exibe lista de clientes
     print ('Escolha o horário de atendimento!') 
-    print ('   1 - diurno') 
-    print ('   2 - Vespertino') 
-    print ('   3 - Noite') 
+    print ('   |1ª| - diurno') 
+    print ('   |2ª| - Vespertino') 
+    print ('   |3ª| - Noite') 
     
-    f = open("coleta.txt", "a") # não exibe lista de clientes
-
+   
     pcoleta=[]  
-    pcoleta = str(input("Digite o horário da coleta:"))
-    pcoleta=pcoleta.upper()
-    tcoleta.append(pcoleta)
-    f.write((pcoleta))
+    coleta = str(input("Digite o horário da coleta: "))
+    coleta=coleta.upper()
+    pcoleta.append(coleta)
+    #f.write((pcoleta))
+    # coleta = []
+    if coleta == '1':
+        coleta = 'Diurno' # intenção adicionar texto
+        pcoleta.append(coleta)
+        print('Horário registrado: ', coleta)
     
-    if pcoleta == '1':
-        pcoleta='Diurno' # intenção adicionar texto
-        tcoleta.append(pcoleta) # error
-        print('Orário registrada')
-    
-    if pcoleta == '2':
-        pcoleta='Vespertino' # intenção adicionar texto
-        tcoleta.append(pcoleta)
-        print('Orário registrada')
+    elif coleta == '2':
+        coleta ='Vespertino' # intenção adicionar texto
+        pcoleta.append(coleta)
+        print('Horário registrado: ', coleta)
 
-    if pcoleta == '3':
-        pcoleta='Noturno' # intenção adicionar texto
-        tcoleta.append(pcoleta)
-        print('Orário registrada')
-    
+    elif coleta == '3':
+        coleta ='Noturno' # intenção adicionar texto
+        pcoleta.append(coleta)
+        print('Horário registrado:', coleta)
+    else:
+        print('Horário não registrado: ')
+
+    f.close()
 
 def listar_coleta():
-    print('Coleta cadastrado para: ',cliente, '\no tipo de coleta e hprario são: ') #exibi cliente solicitou coleta , tipo e horário
-     
-
-
-def remover_coleta():
-    pass
-
-#def atualizar_coleta():
+    f = open("cliente.txt", "r", encoding='utf-8')
+    for x in f:
+        print('Cliente cadastrado:', x)
+    
+    f.close()
+    f = open("coleta.txt", "r", encoding='utf-8')
+    for x in f:
+        print('Tipo de coleta cadastrada: ', x)
+    
+    f.close()
+    f = open("horacoleta.txt", "r", encoding='utf-8')
+    for x in f:
+        print('Horário de coleta cadastrada: ', x)
  
-def hora_coleta():
+    f.close()
 
-    print('Orário registrada', pcoleta) #esse
-    pass 
-
-def verificar_situacao():
+    
+def remover_coleta():
     lcliente=[]
-    cliente = str(input("Digite as novas informações sobre o cliente: "))
-    cliente=cliente.upper()
-    lcliente.append(cliente) # adiciona cliente na lista
-    if cliente in lcliente:
-        lcliente.remove(cliente)
-        print('Alteração realizada com sucesso')
-    else:
-        print('Cadastro não alterado') 
-    pass 
-     
-   
-def le_diario ():
-    ldiario=[]
-    print(ldiario) 
-    pass  
 
-
+    if(os.path.isfile('coleta.txt') == True):
+            cliente = str(input('Digite o nome do cliente: '))
+            cliente=cliente.upper()
+            
+            with  open("coleta.txt", "r")as arquivo: #w apaga a pode escrever
+                linhas = arquivo.readlines()
+            with  open("coleta.txt", "w")as arquivo:
+             
+                for arquivo in linhas:
+                    lcliente.append(cliente)
+                    lcliente = lcliente[0].split(';')
+                    if lcliente != arquivo: #
+                        print('Cliente Excluido')
+                            
+                    else:
+                        print('Solicitação não executada')
+            
+        
 opcao = menu() 
 while opcao != '7': #ensera o aplicativo
     if opcao == '1': #adiciona cliente
         adicionar_cliente() 
-    elif opcao == '2':  #remove cadastro cliente
+
+    elif opcao == '2':  #remove cadastro cliente mas esta escrevendo também 
         remover_cliente() 
         
-    elif opcao == '3': # exiber informações cliente, coleta e horario coleta
-        listar_clientes()
-        # print('clientes cadastrado:',lcliente)
+    elif opcao == '3': 
+      
+        listar_clientes() # exibe lista d e clientes cadastrado
+        
         
     elif opcao == '4': # cadastra coleta
         adicionar_coleta()
 
     elif opcao == '5': # lista soicitações de coleta
-        
         listar_coleta()
-        
-    if opcao == '6': # exclui coleta
-        remover_coleta()
-    
-           
+                        
+    elif opcao == '6': # exclui coleta
+        #cliente=input('Digite nome do cliente')
+        #if cliente== cliente:
+            remover_coleta()
+    else:
+        print('Cliente não encontradp')
+               
     opcao = menu() 
+
 
